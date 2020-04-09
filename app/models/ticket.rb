e239dc6bf1e6092796e9b4b22b2015827fadbf5b
Web Tech <sebastianschlotfeldt@miuandes.cl>
2020-04-09 13:23:24 -0400

    Lab 4 Sebastian Schotfldt

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,12 +2,28 @@ class Ticket < ApplicationRecord
   belongs_to :order
   belongs_to :ticket_type
 
-  # TODO: complete the folowing
-  # before_xxxxx :update_stats
+  after_save :update_stats
+  after_destroy :Update_stats2
+  before_save :tickets_equal_venue_capacity
+
 
   private
     def update_stats
       es = self.ticket_type.event.event_stat
-      # TODO: complete in order to update event stats
+      es.tickets_sold +=1
     end
+  private
+    def update_stats2
+      en = self.ticket_type.event.event_stat
+      en.tickets_sold-=1
+    end
+
+  private
+  def tickets_equal_venue_capacity
+    e = self.ticket_type.event.event_stat
+    f = self.ticket_type.event.event_venue
+    if f.capacity < e.tickets_sold
+      errors.add(:e.tickets_sold, "cant be grater than capacity")
+    end
+  end
 end
--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,3 +1,4 @@
 class TicketType < ApplicationRecord
   belongs_to :event
+  validate :ticket_price, numericality: {grater_than_or_equal_to:0}
 end
